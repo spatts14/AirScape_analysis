f91080699619b5b77822bf96d2ebf4255ec9db6b
Sara Patti <sep22@ic.ac.uk>
2026-06-04 13:41:51 +0100

    correct to parallel

---
+++ src/muspan/cross_PCF_parallel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#PBS -l walltime=12:0:0
+#PBS -l select=1:ncpus=64:mem=512gb
+#PBS -N create_domain_parallel
+#PBS -j oe
+
+# Load production tools
+module load tools/prod
+
+# Load python and bundle
+module load Biopython/1.84-foss-2024a
+
+# Change to directory
+cd /rds/general/user/sep22/home/Projects/AirScape_analysis
+
+# Activate virtual environment
+source muspan/bin/activate
+
+# Run with error logging
+echo "Starting at $(date)"
+
+python src/muspan/cross_PCF_parallel.py
+
+echo "Completed at $(date)"
