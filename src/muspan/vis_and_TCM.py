165930ee70838d0e1e6c9c1f6c1aacc911d9ee1e
Sara Patti <sep22@ic.ac.uk>
2026-06-17 22:46:55 +0100

    iterate through multiple domains

--- src/muspan/vis_and_TCM.py
+++ src/muspan/vis_and_TCM.py
@@ -16,9 +16,8 @@ output_dir = Path(base_dir / "manual_visualizations")
 # Make sure the output directory exists
 output_dir.mkdir(parents=True, exist_ok=True)
 
-# Load domain
-domain = ms.io.load_domain(str(data_dir / "COPD_R009_V2_muspan_domain.muspan"))
-
+# List all domain of domain
+## SHAM
 # COPD_R009
 # COPD_R021
 # COPD_R025
@@ -26,125 +25,133 @@ domain = ms.io.load_domain(str(data_dir / "COPD_R009_V2_muspan_domain.muspan"))
 # COPD_R039
 # COPD_R041
 
-# make domain directory for saving visualizations
-domain_output_dir = output_dir / domain.name
-domain_output_dir.mkdir(parents=True, exist_ok=True)
+## TREATMENT
+list = ["COPD_R011", "COPD_46005", "COPD_R003", "COPD_R014", "COPD_R035", "COPD_R038"]
 
-# Print the unique cell types in the domain
-print(np.unique(domain.labels["Cell Type"]["labels"]))
+for domain_name in list:
+    print(f"Processing domain: {domain_name}")
 
-# Choose two cell types of interest for analysis
-cell1 = "Interstitial macrophages"
-cell2 = "Interstitial macrophages"
-clusters_of_interest = [cell1, cell2]
-# 'Interstitial macrophages', 'Ciliated cells'
-cluster_of_interest_query = ms.query.query(
-    domain, ("label", "Cell Type"), "in", clusters_of_interest
-)
+    # Load domain
+    domain = ms.io.load_domain(str(data_dir / f"{domain_name}_muspan_domain.muspan"))
 
-# Update color if needed
-# Set color for interstitial macrophages
-domain.update_colors(
-    {"Interstitial macrophages": "#f7b7d2"},
-    colors_to_update="labels",
-    label_name="Cell Type",
-)
+    # make domain directory for saving visualizations
+    domain_output_dir = output_dir / domain.name
+    domain_output_dir.mkdir(parents=True, exist_ok=True)
 
+    # Print the unique cell types in the domain
+    print(np.unique(domain.labels["Cell Type"]["labels"]))
 
-# Visualize the domain with cell boundaries
-print("Visualizing the domain with cell boundaries...")
-fig, ax = plt.subplots(figsize=(10, 5))
-ms.visualise.visualise(
-    domain,
-    objects_to_plot=("collection", "Cell boundaries"),
-    add_cbar=False,
-    shape_kwargs={
-        "alpha": 0.5,
-        "linewidth": 0.005,
-        "edgecolor": "#00000000",
-        "color": "#e5e3c4",
-    },
-    ax=ax,
-)
+    # Choose two cell types of interest for analysis
+    cell1 = "Interstitial macrophages"
+    cell2 = "Interstitial macrophages"
+    clusters_of_interest = [cell1, cell2]
+    # 'Interstitial macrophages', 'Ciliated cells'
+    cluster_of_interest_query = ms.query.query(
+        domain, ("label", "Cell Type"), "in", clusters_of_interest
+    )
 
-ms.visualise.visualise(
-    domain,
-    objects_to_plot=cluster_of_interest_query,
-    color_by="Cell Type",
-    ax=ax,
-    marker_size=4,
-    shape_kwargs={"linewidth": 0.4, "alpha": 1},
-    add_scalebar=True,
-    scalebar_kwargs={
-        "size": 1000,
-        "label": "1000µm",
-        "loc": "lower right",
-        "pad": 0.1,
-        "color": "black",
-        "frameon": False,
-        "size_vertical": 2,
-    },
-)
-plt.title(f"Cell Types: {cell1} and {cell2}")
-plt.tight_layout()
-plt.savefig(
-    domain_output_dir / f"cell_types_{cell1}_{cell2}_{domain.name}.png", dpi=300
-)
+    # Update color if needed
+    # Set color for interstitial macrophages
+    domain.update_colors(
+        {"Interstitial macrophages": "#f7b7d2"},
+        colors_to_update="labels",
+        label_name="Cell Type",
+    )
 
+    # Visualize the domain with cell boundaries
+    print("Visualizing the domain with cell boundaries...")
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ms.visualise.visualise(
+        domain,
+        objects_to_plot=("collection", "Cell boundaries"),
+        add_cbar=False,
+        shape_kwargs={
+            "alpha": 0.5,
+            "linewidth": 0.005,
+            "edgecolor": "#00000000",
+            "color": "#e5e3c4",
+        },
+        ax=ax,
+    )
 
-# Calculate TCM
-# compute and visualise the topographical correlation map between points
-print(f"Calculating TCM between {cell1} and {cell2}...")
-TCM_array = ms.spatial_statistics.topographical_correlation_map(
-    domain,
-    population_A=("Cell Type", cell1),
-    population_B=("Cell Type", cell2),
-    mesh_step=5,
-    radius_of_interest=50,
-    kernel_radius=150,
-    kernel_sigma=30,
-    visualise_output=False,
-)
+    ms.visualise.visualise(
+        domain,
+        objects_to_plot=cluster_of_interest_query,
+        color_by="Cell Type",
+        ax=ax,
+        marker_size=4,
+        shape_kwargs={"linewidth": 0.4, "alpha": 1},
+        add_scalebar=True,
+        scalebar_kwargs={
+            "size": 1000,
+            "label": "1000µm",
+            "loc": "lower right",
+            "pad": 0.1,
+            "color": "black",
+            "frameon": False,
+            "size_vertical": 2,
+        },
+    )
+    plt.title(f"Cell Types: {cell1} and {cell2}")
+    plt.tight_layout()
+    plt.savefig(
+        domain_output_dir / f"cell_types_{cell1}_{cell2}_{domain.name}.png", dpi=300
+    )
 
-# Visualize TCM
-print(f"Visualizing TCM between {cell1} and {cell2}...")
-fig, ax = plt.subplots(figsize=(10, 8))
-ms.visualise.visualise(
-    domain,
-    objects_to_plot=("collection", "Cell boundaries"),
-    add_cbar=False,
-    shape_kwargs={
-        "alpha": 0.5,
-        "linewidth": 0.005,
-        "edgecolor": "#00000000",
-        "color": "#bec1c2",
-    },
-    add_scalebar=True,
-    scalebar_kwargs={
-        "size": 1000,
-        "label": "1000µm",
-        "loc": "lower right",
-        "pad": 0.1,
-        "color": "black",
-        "frameon": False,
-        "size_vertical": 2,
-    },
-    ax=ax,
-)
+    # Calculate TCM
+    # compute and visualise the topographical correlation map between points
+    print(f"Calculating TCM between {cell1} and {cell2}...")
+    TCM_array = ms.spatial_statistics.topographical_correlation_map(
+        domain,
+        population_A=("Cell Type", cell1),
+        population_B=("Cell Type", cell2),
+        mesh_step=5,
+        radius_of_interest=50,
+        kernel_radius=150,
+        kernel_sigma=30,
+        visualise_output=False,
+    )
 
-ms.visualise.visualise_topographical_correlation_map(
-    domain,
-    TCM_array,
-    ax=ax,
-    colorbar_limit=None,
-    tcm_cmap="RdBu_r",
-    colorbar_label="TCM",
-)
-plt.title(f"Cell Types: {cell1} and {cell2}")
-plt.tight_layout()
-plt.savefig(
-    domain_output_dir / f"cell_types_{cell1}_{cell2}_{domain.name}_TCM.png", dpi=300
-)
+    # Visualize TCM
+    print(f"Visualizing TCM between {cell1} and {cell2}...")
+    fig, ax = plt.subplots(figsize=(10, 8))
+    ms.visualise.visualise(
+        domain,
+        objects_to_plot=("collection", "Cell boundaries"),
+        add_cbar=False,
+        shape_kwargs={
+            "alpha": 0.5,
+            "linewidth": 0.005,
+            "edgecolor": "#00000000",
+            "color": "#bec1c2",
+        },
+        add_scalebar=True,
+        scalebar_kwargs={
+            "size": 1000,
+            "label": "1000µm",
+            "loc": "lower right",
+            "pad": 0.1,
+            "color": "black",
+            "frameon": False,
+            "size_vertical": 2,
+        },
+        ax=ax,
+    )
 
-print(f"TCM calculation and visualization completed for {cell1} and {cell2}.")
+    ms.visualise.visualise_topographical_correlation_map(
+        domain,
+        TCM_array,
+        ax=ax,
+        colorbar_limit=None,
+        tcm_cmap="RdBu_r",
+        colorbar_label="TCM",
+    )
+    plt.title(f"Cell Types: {cell1} and {cell2}")
+    plt.tight_layout()
+    plt.savefig(
+        domain_output_dir / f"cell_types_{cell1}_{cell2}_{domain.name}_TCM.png", dpi=300
+    )
+
+    print(f"TCM calculation and visualization completed for {cell1} and {cell2}.")
+
 print("Finished!")
