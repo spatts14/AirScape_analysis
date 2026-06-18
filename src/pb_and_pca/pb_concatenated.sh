3438ba95fede934027ad2ae46185245c067ea691
Sara Patti <sep22@ic.ac.uk>
2026-06-18 14:17:06 +0100

    calculate pb on all ROIs and cell types

---
+++ src/pb_and_pca/pb_concatenated.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+#PBS -l walltime=01:00:00
+#PBS -lselect=1:ncpus=1:mem=96gb
+
+# Load production tools
+module load tools/prod
+
+# Load python and bundle
+module load Biopython/1.84-foss-2024a
+
+# Change to directory
+cd /rds/general/user/sep22/home/Projects/AirScape_analysis
+
+# Activate virtual environment
+source airscape_venv/bin/activate
+s
+# Run with error logging
+echo "Starting at $(date)"
+
+# calculate pseudobulk
+python src/pb_and_pca/pb_concatenated.py
+# python src/pb_and_pca/pb_pca.py
+
+echo "Completed at $(date)"
