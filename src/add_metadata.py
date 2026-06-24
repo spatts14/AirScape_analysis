092b80c7e9c1f424f538eb9636398ef4d7b477c6
Sara Patti <sep22@ic.ac.uk>
2026-06-24 10:59:28 +0100

    import colors from utils

--- src/add_metadata.py
+++ src/add_metadata.py
@@ -2,11 +2,15 @@
 
 import logging
 import os
+import sys
 from datetime import datetime
 from pathlib import Path
 
 import pandas as pd
 import scanpy as sc
+
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+from utils.airspace_colors import level_2_palette
 
 
 def setup_logger(log_dir: Path, log_name: str) -> logging.Logger:
@@ -126,50 +130,6 @@ if "treatment_arm" in adata.obs.columns:
 
 
 # Add color palette for treatment arms to adata.uns
-level_2_palette = {
-    # Epithelial
-    "Ciliated cells": "#5B8FA8",
-    "Goblet cells": "#7EB5A6",
-    "Basal cells": "#4A7B6F",
-    "Proliferating Basal cells": "#3D6B5E",
-    "Secretory epithelial cells": "#89C4B0",
-    "AT1 cells": "#A8C5A0",
-    "AT2 cells": "#6B9E78",
-    "Proliferating AT2 cells": "#4E7D5B",
-    # Fibroblasts
-    "Adventitial fibroblasts": "#C4956A",
-    "CTHRC1+ fibroblasts": "#B07D50",
-    "Alveolar fibroblasts": "#D4A97A",
-    "Lipo-fibroblasts": "#E2C49A",
-    # Endothelial
-    "Lymphatic endothelial cells": "#8B7CB3",
-    "Pulmonary vein endothelial cell": "#A08DB8",
-    "Blood endothelial cells - unclassified": "#6B5E9E",
-    "Capillary endothelial cells": "#B8A9CC",
-    "Pulmonary artery endothelial cell": "#7A6EA8",
-    "Pericytes": "#C5B8D6",
-    # Macrophages & monocytes
-    "Macrophages": "#C17B6E",
-    "Lipid-associated macrophages": "#B56355",
-    "Interstitial Macrophages": "#D4957F",
-    "Airway/Alveolar macrophages": "#A05A4A",
-    "Monocytes": "#E8B4A0",
-    # T cells & NK cells
-    "T cells": "#7E9E6E",
-    "CD4+ T cells": "#92B080",
-    "CD8+ T cells": "#5E7E50",
-    "NK cells": "#B5C99A",
-    # Other immune
-    "B cells": "#6E8FAA",
-    "Plasma cells": "#5A7A9A",
-    "Dendritic cells": "#B8A06E",
-    "Mast cells": "#9E8B5A",
-    "Neutrophils": "#D4C07A",
-    # Other
-    "SMC": "#9E9E8A",
-    "Aerocytes": "#7A9E9A",
-    "Unknown": "#A0A0A0",
-}
 adata.uns["level_2_colors"] = [
     level_2_palette[ct] for ct in adata.obs["level_2"].cat.categories
 ]
