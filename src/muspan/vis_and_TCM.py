cfc7b3ed9a7c2cd9e8f04e2493f48e0c25a2bc66
Sara Patti <sep22@ic.ac.uk>
2026-06-29 20:54:28 +0100

    CD8+ T and IM

--- src/muspan/vis_and_TCM.py
+++ src/muspan/vis_and_TCM.py
@@ -138,7 +138,7 @@ def main():
 
     # Choose two cell types of interest for analysis
     cell1 = "CD8+ T cells"
-    cell2 = "CD8+ T cells"
+    cell2 = "Interstitial macrophages"
 
     vmax = 30  # Set the maximum value for the color scale, will do + and - integer
 
