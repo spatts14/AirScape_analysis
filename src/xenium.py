d5aa0922b72400c85d1c93effaea6b8c3b78f1f0
Sara Patti <sep22@ic.ac.uk>
2026-07-01 09:27:24 +0100

    IM and B cells

---
+++ src/xenium.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Load the CSV file into a DataFrame
+df = pd.read_csv(
+    "/Volumes/phenotypingsputumasthmaticsaurorawellcomea1/live/Sara_Patti/009_ST_Xenium/output/muspan/cell_id_to_cluster_labels_level1.csv"
+)
+
+df.head()
