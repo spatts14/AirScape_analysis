14b87bc4c257e219ee14fad6b2caec8923a54e04
Sara Patti <sep22@ic.ac.uk>
2026-07-01 11:55:11 +0100

    mast cells

--- src/muspan/vis_and_TCM.py
+++ src/muspan/vis_and_TCM.py
@@ -138,7 +138,7 @@ def main():
 
     # Choose two cell types of interest for analysis
     cell1 = "Interstitial macrophages"
-    cell2 = "Goblet cells"
+    cell2 = "Mast cells"
 
     vmax = 30  # Set the maximum value for the color scale, will do + and - integer
 
