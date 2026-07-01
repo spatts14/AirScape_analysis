67e520fc08a3962f1f4ab2b01405cdf68b6d38fa
Sara Patti <sep22@ic.ac.uk>
2026-07-01 09:55:49 +0100

    basal cells

--- src/muspan/vis_and_TCM.py
+++ src/muspan/vis_and_TCM.py
@@ -138,7 +138,7 @@ def main():
 
     # Choose two cell types of interest for analysis
     cell1 = "Interstitial macrophages"
-    cell2 = "Mast cells"
+    cell2 = "Basal cells"
 
     vmax = 30  # Set the maximum value for the color scale, will do + and - integer
 
