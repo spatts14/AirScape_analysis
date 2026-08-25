6302a8a8de4e356c8610e2ea13acbc97e48b41db
Sara Patti <sep22@ic.ac.uk>
2026-08-25 11:44:31 +0100

    B cells

--- src/muspan/vis_and_TCM.py
+++ src/muspan/vis_and_TCM.py
@@ -137,8 +137,8 @@ def main():
     logger = setup_logger(log_dir=logs_dir, log_name="vis_TCM")
 
     # Choose two cell types of interest for analysis
-    cell1 = "CD4+ T cells"
-    cell2 = "CD4+ T cells"
+    cell1 = "B cells"
+    cell2 = "B cells"
 
     vmax = 30  # Set the maximum value for the color scale, will do + and - integer
 
