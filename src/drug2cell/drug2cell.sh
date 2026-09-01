6d3216c726e429858dea77554c82f2562e9e5a99
Sara Patti <sep22@ic.ac.uk>
2026-09-01 16:42:29 +0100

    write job to submit to hpc

---
+++ src/drug2cell/drug2cell.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#PBS -l walltime=12:0:0
+#PBS -l select=1:ncpus=4:mem=256gb
+#PBS -N drug2cell
+#PBS -j oe
+
+# Load production tools
+module load tools/prod
+
+# Load python and bundle
+module load Biopython/1.84-foss-2024a
+
+# Change to directory
+cd /rds/general/user/sep22/home/Projects/AirScape_analysis
+
+# Activate virtual environment
+source muspan/bin/activate
+
+# Run with error logging
+echo "Starting at $(date)"
+
+python src/drug2cell/drug2cell_IPF.py
+
+echo "Completed at $(date)"
