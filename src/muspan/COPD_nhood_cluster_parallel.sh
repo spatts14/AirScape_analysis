ffe6cbac3a3cb05db347d2ac41c7fcfbcd6c5b37
Sara Patti <sep22@ic.ac.uk>
2026-09-15 17:05:04 +0100

    COPD nhood

---
+++ src/muspan/COPD_nhood_cluster_parallel.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#PBS -J 0-3
+#PBS -l select=1:ncpus=4:mem=256gb
+#PBS -l walltime=24:00:0
+#PBS -N COPD_nhood_cluster
+#PBS -j oe
+
+# Load production tools
+module load tools/prod
+
+# Load python and bundle
+module load Biopython/1.84-foss-2024a
+
+# Change to directory
+cd /rds/general/user/sep22/home/Projects/AirScape_analysis
+
+# Activate virtual environment
+source muspan/bin/activate
+
+# Print start time
+echo "Starting at $(date)"
+
+# Iterate over the number of clusters to use for clustering
+CLUSTER_LIST=(12 14 16 18 20)
+NUMBER_OF_CLUSTERS=${CLUSTER_LIST[$PBS_ARRAY_INDEX]}
+
+echo "Starting cluster count $NUMBER_OF_CLUSTERS at $(date) (array index $PBS_ARRAY_INDEX)"
+python src/muspan/nhood_cluster_COPD.py --number_of_clusters "$NUMBER_OF_CLUSTERS"
+echo "Completed at $(date)"
